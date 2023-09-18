fb7e1406bcefd1c7f63d2b2b8fcc3a9074269ee5
givemeapassword <vt070502@yandex.ru>
2023-09-18 23:31:20 +0300

    полное изменение дизайна

--- app/src/main/java/com/example/fakepedometr/MainActivity.kt
+++ app/src/main/java/com/example/fakepedometr/MainActivity.kt
@@ -1,44 +1,62 @@
 package com.example.fakepedometr
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.renderscript.ScriptGroup.Binding
+import android.text.Layout
+import android.view.View
+import android.widget.LinearLayout.LayoutParams
+import android.widget.LinearLayout.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isInvisible
+
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
+import androidx.fragment.app.FragmentManager
 import com.example.fakepedometr.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var constraintLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
+        constraintLayout = findViewById(R.id.main)
 
-        binding.bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId){
-                R.id.Home -> replaceFragment(Home())
-                R.id.Settings -> replaceFragment(Setting())
+            binding.bottomNavigationView.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.Home -> {
 
-                else -> {
+                        constraintLayout.visibility = View.VISIBLE
+                        replaceFragment(Home())
+                    }
+
+                    R.id.Settings -> {
+                        constraintLayout.visibility = View.INVISIBLE
+                        replaceFragment(Setting())
+                    }
+
+                    else -> {
+
+                    }
 
                 }
-
+                true
             }
-            true
-        }
 
     }
 
 
     private fun replaceFragment(fragment: Fragment){
-
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main,fragment)
+        fragmentTransaction.replace(R.id.main, fragment)
         fragmentTransaction.commit()
     }
-}
+
+
+    }
+
